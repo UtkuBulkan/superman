ba3d466de3d0da2babaac7922d0e1a1817408310
Utku Bulkan <bulkanutku@gmail.com>
2019-01-23 23:14:44 +0000

    merging develop to master

---
+++ camera/CameraManager.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 SU Technology Ltd. All rights reserved.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 SU Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#include <memory>
+#include <list>
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "CameraManager.h"
+#include "syslog_cpp.h"
+
+CameraManager::CameraManager()
+{
+	logger << syslog::level::info << "CameraManager Constructor" << std::endl;
+	cv::namedWindow("Camera1", cv::WINDOW_NORMAL);
+	cv::resizeWindow("Camera1", 640, 480);
+}
+
+CameraManager::~CameraManager()
+{
+
+}
+
+void CameraManager::add_camera()
+{
+	Camera *camera_view = new Camera("/home/utku/superman/testcontent/single/carvideo.mp4");
+	camera_collection.push_front(camera_view);
+}
+
+void CameraManager::loop()
+{
+	cv::Mat frame;
+	while(1) {
+		logger << syslog::level::info << "Timestamp" << std::endl;
+		for(std::list<Camera*>::iterator it=camera_collection.begin(); it != camera_collection.end(); it++) {
+			frame = (*it)->get_camera_frame();
+			object_detector.process_frame(frame);
+			cv::imshow("Camera1", frame);
+			if(cv::waitKey(30) >= 0) break;
+		}
+	}
+}
+
---
+++ manager/Manager.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 SU Technology Ltd. All rights reserved.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 SU Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#include <iostream>
+#include "CameraManager.h"
+
+#include "syslog_cpp.h"
+syslog::syslog_ostream logger;
+
+int main()
+{
+	CameraManager a;
+
+	logger.setlogmask(syslog::level::debug);
+	logger.openlog("superman");
+
+	logger << syslog::level::debug << "Manager main is instantiated." << std::endl;
+	a.add_camera();
+	logger << syslog::level::debug << "Getting into camera loop\n" << std::endl;
+	a.loop();
+
+	logger.closelog();
+
+	return 0;
+}
